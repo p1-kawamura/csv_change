81627cb0d4c1145ccca6cb2ec8030dff47430ec9
p1-kawamura <kawamura@p1-intl.com>
2024-04-26 09:51:46 +0900

    com0951

--- csv_app/forms.py
+++ csv_app/forms.py
@@ -6,6 +6,6 @@ class Masterform(forms.Form):
     jan=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control","readonly":"readonly"}))
     hinban=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
     kataban=forms.ChoiceField(
-        choices=[("",""),("1","01"),("2","02"),("3","03"),("4","04"),("7","07")],     
+        choices=[("",""),("1","01"),("2","02"),("3","03"),("4","04"),("5","05"),("7","07")],     
         widget=forms.Select(attrs={"class":"form-select"})
     )
